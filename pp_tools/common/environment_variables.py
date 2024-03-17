7b68bda00addb53da5fc7e1cfa42d1b7059c9bf3
Jose Alonzo <pepemxl@gmail.com>
2024-03-17 14:52:23 -0600

    Adding local configs

--- pp_tools/common/environment_variables.py
+++ pp_tools/common/environment_variables.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import os
 import yaml
 if __name__  == '__main__':
@@ -5,6 +6,40 @@ if __name__  == '__main__':
     package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
     sys.path.append(package_path)
 from pp_tools.common.constants import CONFIG_FILE_PATH
+from pp_tools.common.utils.logger import get_logger
+
+
+log = get_logger(__file__, "INFO")
+FLAG_ENV_VARS_LOADED = False
+
+
+def load_environment_variables_from_local_config():
+    global FLAG_ENV_VARS_LOADED
+    if os.path.isfile(CONFIG_FILE_PATH):
+        with open(CONFIG_FILE_PATH, "r") as ptr_file:
+            env_data = yaml.safe_load(ptr_file)
+            config = env_data.get("data")
+            os.environ.update(config)
+            FLAG_ENV_VARS_LOADED = True
+    else:
+        log.error("Config file \"{0}\" was not found!".format(CONFIG_FILE_PATH))
+        sys.exit(1)
+
+
+def get_env_var(env_var_key: str) -> Optional[str]:
+    global FLAG_ENV_VARS_LOADED
+    if not FLAG_ENV_VARS_LOADED:
+        load_environment_variables_from_local_config()
+    try:
+        value = os.getenv(env_var_key)
+    except Exception as e:
+        log.exception("Exception with environment variable with key: {0} Exception:{1}".format(env_var_key, str(e)))
+    if value is None:
+        log.error("Environment variable with key: {0} was not found!".format(env_var_key))
+        return value
+    return value
+
+
 
 
 if __name__ == '__main__':
--- pp_tools/common/utils/logger.py
+++ pp_tools/common/utils/logger.py
@@ -4,15 +4,18 @@ import os
 from pp_tools.common.constants import LOGS_PATH
 
 
-
 def get_logger(logger_name: str = "pp_tools", level: str = "INFO", logger_filename = "pp_tools", flag_stdout: bool = False) -> logging.Logger:
     now = datetime.now()
     sufix = now.strftime("%Y_%m_%d")
     filename = os.path.basename(logger_filename)
+    filename_base = filename
     if filename.find(".py") > 0:
         filename_base = filename[:-3]
     _logger_filename = "{0}_{1}.log".format(filename_base, sufix)
-    logger_full_filename = os.path.join(LOGS_PATH, _logger_filename)
+    if os.path.isdir(LOGS_PATH):
+        logger_full_filename = os.path.join(LOGS_PATH, _logger_filename)
+    else:
+        flag_stdout = True
     if flag_stdout:
         logging.basicConfig(
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -20,8 +23,8 @@ def get_logger(logger_name: str = "pp_tools", level: str = "INFO", logger_filena
     else:
         logging.basicConfig(
             filename=logger_full_filename,
-            filemode='a+'
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            filemode='a+',
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         )
     logger = logging.getLogger(logger_name)
     LEVEL = logging._nameToLevel.get('INFO')
